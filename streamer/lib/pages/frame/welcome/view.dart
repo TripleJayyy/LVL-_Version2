3667b1112253c53fcab4753b2348552367a6e641
TripleJah <orangehedge@protonmail.com>
2023-02-23 14:42:06 -0600

    routing for bootpages

--- streamer/lib/pages/frame/welcome/view.dart
+++ streamer/lib/pages/frame/welcome/view.dart
@@ -1,14 +1,32 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:streamer/common/values/colors.dart';
+import 'package:streamer/pages/frame/welcome/controller.dart';
+import 'package:get/get.dart';
 
-class WelcomePage extends StatelessWidget {
+class WelcomePage extends GetView<WelcomeController> {
   const WelcomePage({Key? key}) : super(key: key);
 
   Widget _buildPageHeadTitle(String title) {
-    return Container();
+    return Container(
+      child: Text(title,
+          style: TextStyle(
+              color: AppColors.primaryElementText,
+              fontFamily: "Montserrat",
+              fontWeight: FontWeight.bold,
+              fontSize: 45
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        child: _buildPageHeadTitle(controller.title),
+      ),
+    );
   }
 }
