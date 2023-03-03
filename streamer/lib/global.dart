c04d2c7295f8952d9377f3c879b3788859ba6add
TripleJah <orangehedge@protonmail.com>
2023-03-03 16:52:26 -0600

    global widget

--- streamer/lib/global.dart
+++ streamer/lib/global.dart
@@ -1,12 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:streamer/common/services/services.dart';
 import 'package:streamer/common/store/store.dart';
 import 'package:get/get.dart';
+import 'package:streamer/firebase_options.dart';
 
 class Global{
   static Future init() async {
     WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+
     await Get.putAsync<StorageService>(() => StorageService().init());
     Get.put<UserStore>(UserStore());
   }
