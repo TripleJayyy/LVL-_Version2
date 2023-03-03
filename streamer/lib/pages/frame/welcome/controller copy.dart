7220657f686e13a65504906d494c8dd9b919bc58
TripleJah <orangehedge@protonmail.com>
2023-03-03 16:44:09 -0600

    controllers updated

---
+++ streamer/lib/pages/frame/welcome/controller copy.dart
@@ -0,0 +1,14 @@
+import 'package:get/get.dart';
+import 'package:streamer/pages/frame/welcome/state.dart';
+
+class WelcomeController extends GetxController{
+  WelcomeController();
+  final title = "LVL Stream .";
+  final state  = WelcomeState();
+
+  @override
+  void onReady(){
+    super.onReady();
+    print("Welcome controller");
+  }
+}
---
+++ streamer/lib/pages/frame/welcome/view copy.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:streamer/common/values/colors.dart';
+import 'package:streamer/pages/frame/welcome/controller.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class WelcomePage extends GetView<WelcomeController> {
+  const WelcomePage({Key? key}) : super(key: key);
+
+  Widget _buildPageHeadTitle(String title) {
+    return Container(
+      margin: EdgeInsets.only(top: 350),
+      child: Text(
+        title,
+        textAlign: TextAlign.center,
+        style: TextStyle(
+            color: AppColors.primaryElementText,
+            fontFamily: "Montserrat",
+            fontWeight: FontWeight.bold,
+            fontSize: 45.sp
+            ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.primaryElement,
+      body: Container(
+        width: 360.w,
+        height: 780.h,
+        child: _buildPageHeadTitle(controller.title),
+      ),
+    );
+  }
+}
