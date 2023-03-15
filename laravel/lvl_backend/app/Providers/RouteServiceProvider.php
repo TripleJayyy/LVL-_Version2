72c088df35716100f6c0d9aed4198d3208a7b66f
TripleJah <orangehedge@protonmail.com>
2023-03-15 16:55:56 -0600

    working api for laravel validation

--- laravel/lvl_backend/app/Providers/RouteServiceProvider.php
+++ laravel/lvl_backend/app/Providers/RouteServiceProvider.php
@@ -2,10 +2,7 @@
 
 namespace App\Providers;
 
-use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
 
 class RouteServiceProvider extends ServiceProvider
@@ -17,32 +14,57 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    protected $namespace = 'App\Http\Controllers';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
+     * 
+     * @return void
      */
-    public function boot(): void
+    public function boot()
     {
-        $this->configureRateLimiting();
+        //
+    
+        parent::boot();
 
-        $this->routes(function () {
-            Route::middleware('api')
-                ->prefix('api')
-                ->group(base_path('routes/api.php'));
-
-            Route::middleware('web')
-                ->group(base_path('routes/web.php'));
-        });
     }
 
     /**
      * Configure the rate limiters for the application.
+     * 
+     * @return void
      */
-    protected function configureRateLimiting(): void
-    {
-        RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
-        });
+    public function map(){
+        $this->mapApiRoutes();
+
+        $this->mapWebRoutes();
+
+        //
+    }
+
+    /** web routes for app
+     * 
+     * csrf protection, etc.
+     * 
+     * @return void
+     */
+    protected function mapWebRoutes(){
+        Route::middleware('web')
+            ->namespace($this->namespace)
+            ->group(base_path('routes/web.php'));
+    }
+
+    /**
+     * api routes
+     * 
+     * typically stateless
+     * 
+     * @return void
+     */
+    protected function mapApiRoutes(){
+        Route::prefix('api')
+        ->middleware('api')
+        ->namespace($this->namespace)
+        ->group(base_path('routes/api.php'));
     }
 }
