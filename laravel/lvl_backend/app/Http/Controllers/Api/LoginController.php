b1a028702e55f037d2065a5f81c231bfcfee716c
TripleJah <orangehedge@protonmail.com>
2023-03-15 18:45:16 -0600

    laravel login controller finished

--- laravel/lvl_backend/app/Http/Controllers/Api/LoginController.php
+++ laravel/lvl_backend/app/Http/Controllers/Api/LoginController.php
@@ -1,9 +1,15 @@
 <?php
 
 namespace App\Http\Controllers\Api;
+use Exception;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Carbon;
+use Illuminate\Validation\Rule;
+
+
 
 class LoginController extends Controller {
 
@@ -20,8 +26,48 @@ class LoginController extends Controller {
 
         if($validator->fails()){
             return ['code'=>-1, "data"=>"no valid data", 'msg'=>$validator->errors()->first()];
+        }
+
+        $validated = $validator->validated();
+        $map = [];
+        $map['type'] = $validated['type'];
+        $map['open_id'] = $validated['open_id'];
+        $result = DB::table('users')->select(
+            'avatar',
+            'name',
+            'description',
+            'type', 
+            'token', 
+            'access_token',
+            'online')
+            ->where($map)->first();
+        if(empty($result)){
+            $validated['token'] = md5(uniqid().rand(10000,99999));
+            $validated['created_at'] = Carbon::now();
+            $validated['access_token'] = md5(uniqid().rand(1000000, 9999999));
+            $validated['expire_date'] = Carbon::now()->addDays(30);
+            $user_id = DB::table('users')->insertGetId($validated);
+            $user_result = DB::table('users')->select(
+                'avatar',
+                'name',
+                'description',
+                'type', 
+                'token', 
+                'access_token',
+                'online')->where('id', '=', $user_id)->first();
+
+            return ['code'=>0, 'data'=>$user_result, 'msg'=>'no user found'];
         }else{
-            return ['code'=>1, "data"=>"valid data", 'msg'=>"success"];
+            $access_token= md5(uniqid().rand(1000000, 9999999));
+            $expire_date = Carbon::now()->addDays(30);
+            DB::table("users")->where($map)->update(
+                [
+                    "access_token"=>$access_token,
+                    "expire_date"=>$expire_date
+                ]
+            );
+            $result->access_token= $access_token;
+            return ['code'=>1, 'data'=>$result, 'msg'=>'User information updated'];
         }
     }
 }
